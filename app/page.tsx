304aeef99e2b70accae4734d8cb5c2a8cdcd8b4b
JereD1 <omoruyijeremiah3@gmail.com>
2025-06-26 18:45:48 +0100

    new version

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import Header from "./components/header";
-import Context from './components/context'
-
-
 
 export default function Home() {
   return (
     
     <main >
-      <Header />
-      < Context />
-         
+     
     </main>
   
   );
