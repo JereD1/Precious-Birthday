da9c26757a4a6fe97e74175308c30ebb5463e65a
JereD1 <omoruyijeremiah3@gmail.com>
2025-06-28 03:11:18 +0100

    update v3.20393i

--- app/middleware.ts
+++ app/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 // Create a route matcher for public routes
 const isPublicRoute = createRouteMatcher([
   '/login(.*)',  // Allow the login route
-  '/signup(.*)', // Allow the sign-up route
+  '/sign-up(.*)', // Allow the sign-up route
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
